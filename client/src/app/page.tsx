d92e2a3064d07a40b4b68faabaa3835fd81dfc22
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-05-26 20:49:41 +0900

    test

---
+++ client/src/app/page.tsx
@@ -0,0 +1,11 @@
+async function getHello() {
+  const res = await fetch(`http://localhost:8080`);
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await getHello();
+  console.log(data);
+
+  return <h1>{data.text}</h1>;
+}
